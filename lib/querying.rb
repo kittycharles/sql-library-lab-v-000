c7afc120055c3b9008a0665604f9208fa10de552
Richard Sanchez <richard.charles.s@gmail.com>
2018-09-12 15:16:57 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -8,8 +8,8 @@ end
 
 def select_name_and_motto_of_char_with_longest_motto
   "SELECT name, motto FROM characters
-    HAVING COUNT(motto) DSC
-    LIMIT 1"
+    HAVING COUNT(motto) DESC
+    LIMIT 1;"
 end
 
 
