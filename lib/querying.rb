98fef76f040c9cad45b0d4297be053c312ceb6b4
Richard Sanchez <richard.charles.s@gmail.com>
2018-09-12 15:26:50 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -34,7 +34,7 @@ def select_series_title_with_most_human_characters
         ON series.id = characters.series_id
       WHERE characters.species = 'human'
       GROUP BY characters.series_id
-      ORDER BY num_species DESC
+      ORDER BY COUNT(characters.series_id) DESC
       LIMIT 1;"
 end
 
