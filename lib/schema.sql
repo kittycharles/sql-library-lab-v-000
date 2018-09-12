55395ac32157618ef2d3e8ea0a2cf444bcc64d27
Richard Sanchez <richard.charles.s@gmail.com>
2018-09-12 14:52:48 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -2,24 +2,24 @@ CREATE TABLE series (
         id INTEGER PRIMARY KEY,
                 title TEXT,
                 author_id INTEGER,
-                subgenre_id INTEGER,
+                subgenre_id INTEGER
             );
 
 CREATE TABLE subgenres (
         id INTEGER PRIMARY KEY,
-                name TEXT,
+                name TEXT
             );
 
 CREATE TABLE authors (
         id INTEGER PRIMARY KEY,
-                name TEXT,
+                name TEXT
             );
 
 CREATE TABLE books (
         id INTEGER PRIMARY KEY,
                 title TEXT,
                 year INTEGER,
-                series_id INTEGER,
+                series_id INTEGER
             );
 
 CREATE TABLE characters (
@@ -27,8 +27,8 @@ CREATE TABLE characters (
                 name TEXT,
                 species TEXT,
                 motto TEXT,
-                series_id INTEGER
-                author_id INTEGER,
+                series_id INTEGER,
+                author_id INTEGER
             );
 
 CREATE TABLE character_books (
